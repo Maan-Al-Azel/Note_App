e1df749c8dd9257b59fc73c40117d35c966145b0
maan-al-azel <emomlovee@gmail.com>
2025-11-28 00:46:23 +0300

    Fix git state

--- lib/widget/custom_app_bar.dart
+++ lib/widget/custom_app_bar.dart
@@ -5,21 +5,25 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(children:  [
-      Text('Notes',style: TextStyle(fontSize: 32),)
-    ]);
+    return const Row(
+      children: [
+        Text('Notes', style: TextStyle(fontSize: 32)),
+        SizedBox(width: 200),
+        CustomSearchIcon(),
+      ],
+    );
   }
 }
-
 
 class CustomSearchIcon extends StatelessWidget {
   const CustomSearchIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       height: 45,
       width: 45,
+      child: const Icon(Icons.search, size: 28),
     );
   }
 }
