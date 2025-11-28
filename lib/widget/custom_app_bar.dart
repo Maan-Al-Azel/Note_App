be7ba27d4843253770888b40569cbcdc7baf1be5
maan-al-azel <emomlovee@gmail.com>
2025-11-29 00:39:15 +0300

    add Note item

--- lib/widget/custom_app_bar.dart
+++ lib/widget/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widget/Custom_Search_Icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -8,22 +9,10 @@ class CustomAppBar extends StatelessWidget {
     return const Row(
       children: [
         Text('Notes', style: TextStyle(fontSize: 32)),
-        SizedBox(width: 200),
+        Spacer(),
         CustomSearchIcon(),
       ],
     );
   }
 }
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: 45,
-      child: const Icon(Icons.search, size: 28),
-    );
-  }
-}
